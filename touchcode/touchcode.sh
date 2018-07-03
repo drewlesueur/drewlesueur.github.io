5c593d4d135c60aee9f7a16ec9b9dc4976f19814
Drew LeSueur <drewalex@gmail.com>
2018-07-03 08:00:12 +0000

    ideas for touchcode

--- touchcode/touchcode.sh
+++ touchcode/touchcode.sh
@@ -5,10 +5,20 @@ myfiles() {
 }
 
 myupdir() {
+    echo $'Content-Type: text/html\r'
+    echo $'\r'
     cd ..
     ls
 }
 
 mygotodir() {
     cd $1
+    ls
+}
+
+
+mygitstatus() {
+    echo $'Content-Type: text/html\r'
+    echo $'\r'
+    git status
 }
