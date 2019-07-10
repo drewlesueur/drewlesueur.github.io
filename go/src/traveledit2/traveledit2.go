d604fce8acb000b2824844e1af5a4dc67e5e557e
Drew LeSueur <drewalex@gmail.com>
2019-07-10 04:43:07 +0000

    go fmt

--- go/src/traveledit2/traveledit2.go
+++ go/src/traveledit2/traveledit2.go
@@ -46,51 +46,51 @@ func main() {
 	certFile := os.Getenv("CERTFILE")
 	keyFile := os.Getenv("KEYFILE")
 	flag.Parse()
-    log.Printf("certFile: %s", certFile)
-    log.Printf("keyfile: %s", keyFile)
+	log.Printf("certFile: %s", certFile)
+	log.Printf("keyfile: %s", keyFile)
 
- if (*location == "") {
-	  cmd := exec.Command("bash", "-c", "pwd")
-	  ret, err := cmd.Output()
-	  if err != nil {
-	    log.Fatal("could not get cwd")
-	  }
-   *location = strings.TrimSpace(string(ret))
- }
- log.Printf("location: %s", *location)
+	if *location == "" {
+		cmd := exec.Command("bash", "-c", "pwd")
+		ret, err := cmd.Output()
+		if err != nil {
+			log.Fatal("could not get cwd")
+		}
+		*location = strings.TrimSpace(string(ret))
+	}
+	log.Printf("location: %s", *location)
 	mux := http.NewServeMux()
 	// fs := http.FileServer(http.Dir("./public"))
 	//mux.Handle("/", http.StripPrefix("/", fs))
 
-  mux.HandleFunc("/yo", func(w http.ResponseWriter, r *http.Request) {
-    http.ServeFile(w, r, "./public/yo.html")  
-  })
-  mux.HandleFunc("/yo/", func(w http.ResponseWriter, r *http.Request) {
-    log.Printf("the yo path: %s", r.URL.Path)
-    http.ServeFile(w, r, "./public/yo.html")  
-  })
+	mux.HandleFunc("/yo", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "./public/yo.html")
+	})
+	mux.HandleFunc("/yo/", func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("the yo path: %s", r.URL.Path)
+		http.ServeFile(w, r, "./public/yo.html")
+	})
 	mux.HandleFunc("/mybash", func(w http.ResponseWriter, r *http.Request) {
-	  cmdString := r.FormValue("cmd")
-	  if cmdString == "" {
-	    cmdString = ":"
-	  }
-	  cwd := r.FormValue("cwd") // current working directory
-	  
-   // add the cwd so the client can remember it
-	  cmdString = "cd "+cwd+";\n"+cmdString+ ";\necho ''; pwd" 
-	  
-	  log.Printf("the command we want is: %s", cmdString)
-	  cmd := exec.Command("bash", "-c", cmdString)
-	  ret, err := cmd.CombinedOutput()
-	  if err != nil {
-	    log.Printf("there was and error running command: %s", cmdString)
-	    http.Error(w, err.Error(), http.StatusInternalServerError)
-	    return
-	  }
-	  //lines := strings.Split(string(r), "\n")
-	  
-	  log.Printf("the combined output of the command is: %s", string(ret))
-	  w.Write(ret)
+		cmdString := r.FormValue("cmd")
+		if cmdString == "" {
+			cmdString = ":"
+		}
+		cwd := r.FormValue("cwd") // current working directory
+
+		// add the cwd so the client can remember it
+		cmdString = "cd " + cwd + ";\n" + cmdString + ";\necho ''; pwd"
+
+		log.Printf("the command we want is: %s", cmdString)
+		cmd := exec.Command("bash", "-c", cmdString)
+		ret, err := cmd.CombinedOutput()
+		if err != nil {
+			log.Printf("there was and error running command: %s", cmdString)
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		//lines := strings.Split(string(r), "\n")
+
+		log.Printf("the combined output of the command is: %s", string(ret))
+		w.Write(ret)
 	})
 	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		if strings.Contains("..", r.URL.Path) {
@@ -100,21 +100,21 @@ func main() {
 		if r.Method == "GET" {
 			var c []byte
 			if r.URL.Path == "/" {
-			  c = []byte{}
+				c = []byte{}
 			} else {
-			  parts := strings.Split(r.URL.Path, ",")
-			  c2, err := ioutil.ReadFile(*location + "/" + parts[0][1:])
-			  if err != nil {
-			  	http.Error(w, "error reading r equested file", http.StatusInternalServerError)
-			  	return
-			  }
-			  c = c2
+				parts := strings.Split(r.URL.Path, ",")
+				c2, err := ioutil.ReadFile(*location + "/" + parts[0][1:])
+				if err != nil {
+					http.Error(w, "error reading r equested file", http.StatusInternalServerError)
+					return
+				}
+				c = c2
 			}
 			if r.FormValue("raw") == "1" {
-			  w.Write(c)
-			  return
+				w.Write(c)
+				return
 			}
-			
+
 			b, err := ioutil.ReadFile(*indexFile)
 			if err != nil {
 				http.Error(w, "error reading index file", http.StatusInternalServerError)
@@ -125,15 +125,15 @@ func main() {
 			contentLines := strings.Split(contentString, "\n")
 			contentLinesJSON, err := json.MarshalIndent(contentLines, "", " ")
 			contentLinesJSONString := string(contentLinesJSON)
-			htmlString = strings.Replace(htmlString, "// ROOTLOCATION GOES HERE", "var rootLocation = \""+*location + "\"", 1)
+			htmlString = strings.Replace(htmlString, "// ROOTLOCATION GOES HERE", "var rootLocation = \""+*location+"\"", 1)
 			htmlString = strings.Replace(htmlString, "// LINES GO HERE", "var lines = "+contentLinesJSONString, 1)
-			
+
 			// TODO: when bash mode is disabled, don't do this part.
 			log.Printf("yea I set rootLocation to be: %s", *location)
 			if r.FormValue("src") != "1" {
 				w.Header().Set("Content-Type", "text/html")
 			}
-            ioutil.WriteFile("tmp", []byte(htmlString), 0777)
+			ioutil.WriteFile("tmp", []byte(htmlString), 0777)
 			fmt.Fprintf(w, "%s", htmlString)
 		} else if r.Method == "POST" {
 			content := r.FormValue("content")
@@ -145,7 +145,7 @@ func main() {
 				return
 			}
 			s := SaveResponse{}
-			err := ioutil.WriteFile(*location + "/" + r.URL.Path[1:], []byte(content), 0644)
+			err := ioutil.WriteFile(*location+"/"+r.URL.Path[1:], []byte(content), 0644)
 			if err != nil {
 				s.Error = err.Error()
 			} else {
